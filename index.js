8464c465e4509e65454732f66ace97bbfc47ba8f
ellewilson96 <eleanorwilsonn@gmail.com>
2019-03-29 23:29:40 -0400

    Begin writing jquery selector functions

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 
 $(function() {
+  attachEventListeners();
 
-  $('#change-color').click(function() {
-    alert("You clicked me")
+function getPaintings(e) {
+  e.preventDefault();
+    $.get('https://collectionapi.metmuseum.org/public/collection/v1/objects', function(data) {
+      console.log(data)
+      data.forEach(paintingNew => {
+        const painting = paintingNew
+        const id = painting.id
+        const paintingHtml = `
+        <div class="painting-object">
+          <img src="${painting.primaryImage}"><br>
+          <h1>${painting.title}</h1>
+        </div>
+        `
+      $('div#content').append(paintingHtml)
   })
+}, "json")
+}
+
+function attachEventListeners() {
+
+    $('a#refresh').on('click', getPaintings)
+}
 })
